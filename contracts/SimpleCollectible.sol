eaa5960792f8097563eb91d9be28931b8d2df4da
mehmetatakancavuslu <atakancavuslu322@gmail.com>
2022-01-07 19:48:45 +0300

    Implement simpleCollectible.sol

--- contracts/SimpleCollectible.sol
+++ contracts/SimpleCollectible.sol
@@ -1,8 +1,24 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.6.6;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
+// @dev this is the factory contract
 contract SimpleCollectible is ERC721 {
-    constructor() public ERC721("Doggie", "DOG") {}
+    uint256 public tokenCounter;
+
+    constructor() public ERC721("Doggie", "DOG") {
+        tokenCounter = 0;
+    }
+
+    function createCollectible(string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        uint256 newTokenId = tokenCounter;
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        tokenCounter = tokenCounter + 1;
+        return newTokenId;
+    }
 }
