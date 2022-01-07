10848a1c89d4ee69000f34c668396a3b7990ed98
mehmetatakancavuslu <atakancavuslu322@gmail.com>
2022-01-07 19:22:56 +0300

    added image.

---
+++ contracts/SimpleCollectible.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract SimpleCollectible is ERC721 {
+    constructor() public ERC721("Doggie", "DOG") {}
+}
